6d90fa28597d2e6d82b826ae1374dd3a58fad955
mingodad <mingodad@gmail.com>
2018-11-28 12:44:57 +0100

    Fix from sqlite3/tools/lemon

--- lua2ljs/lempar.c
+++ lua2ljs/lempar.c
@@ -23,6 +23,7 @@
 ** input grammar file:
 */
 #include <stdio.h>
+#include <assert.h>
 /************ Begin %include sections from the grammar ************************/
 %%
 /**************** End of %include directives **********************************/
@@ -993,10 +994,9 @@ int Parse(
         yymajor = YYNOCODE;
       }else{
         while( yypParser->yytos >= yypParser->yystack
-            && yymx != YYERRORSYMBOL
             && (yyact = yy_find_reduce_action(
                         yypParser->yytos->stateno,
-                        YYERRORSYMBOL)) >= YY_MIN_REDUCE
+                        YYERRORSYMBOL)) > YY_MAX_SHIFTREDUCE
         ){
           yy_pop_parser_stack(yypParser);
         }
